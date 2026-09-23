1bac8cdcd127e368c066a4f50a75efbcfa69b6df
joreid-r7 <josh_reid@rapid7.com>
2026-09-23 23:08:28 +0100

    added some more

---
+++ order/src/main/java/com/ecommerce/order/client/ProductServiceClientConfig.java
@@ -0,0 +1,7 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2012-2026, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+package com.ecommerce.order.client;public class ProductServiceClientConfig {
+}
